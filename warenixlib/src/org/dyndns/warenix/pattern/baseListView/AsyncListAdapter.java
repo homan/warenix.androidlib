1cbc03e606f954983f558e589ed48683d95f7936
Warren Wong <warren.wong@enterproid.com>
2012-03-18 02:45:54 +0800

    Add custom logcat WLog that print which app it is
    Add AsyncListAdapter to support load dataList asynchronously

---
+++ warenixlib/src/org/dyndns/warenix/pattern/baseListView/AsyncListAdapter.java
@@ -0,0 +1,109 @@
+package org.dyndns.warenix.pattern.baseListView;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.ListView;
+
+public class AsyncListAdapter extends ListViewAdapter {
+	private AsyncRefreshTask mAsyncRefreshTask;
+	private AsyncRefreshListener mAsyncRefreshListener;
+	private BackgroundLogic mBackgroundLogic;
+
+	public AsyncListAdapter(Context context, ListView listView) {
+		super(context, listView);
+	}
+
+	public void setAsyncRefreshListener(AsyncRefreshListener listener) {
+		mAsyncRefreshListener = listener;
+	}
+
+	public void setBackgroundLogic(BackgroundLogic logic) {
+		mBackgroundLogic = logic;
+	}
+
+	public void asyncRefresh() {
+		listView.post(new Runnable() {
+
+			@Override
+			public void run() {
+				itemList.clear();
+				notifyDataSetChanged();
+			}
+
+		});
+
+		mAsyncRefreshTask = new AsyncRefreshTask(mAsyncRefreshListener,
+				mBackgroundLogic);
+		mAsyncRefreshTask.execute();
+
+	}
+
+	public void cancelAsyncRefresh() {
+		if (mAsyncRefreshTask != null && !mAsyncRefreshTask.isCancelled()) {
+			mAsyncRefreshTask.cancel(true);
+		}
+	}
+
+	private static class AsyncRefreshTask extends AsyncTask<Void, Void, Void> {
+		AsyncRefreshListener mAsyncRefreshListener;
+		BackgroundLogic mBackgroundLogic;
+		Object result;
+
+		public AsyncRefreshTask(AsyncRefreshListener listener,
+				BackgroundLogic logic) {
+			mAsyncRefreshListener = listener;
+			mBackgroundLogic = logic;
+		}
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			if (mAsyncRefreshListener != null) {
+				mAsyncRefreshListener.onAysncRefreshStarted();
+			}
+			if (mBackgroundLogic != null) {
+				result = mBackgroundLogic.doInBackground();
+			}
+			return null;
+		}
+
+		protected void onPostExecute(Void v) {
+			if (mAsyncRefreshListener != null) {
+				mAsyncRefreshListener.onAysncRefreshEnded();
+			}
+			if (mBackgroundLogic != null) {
+				mBackgroundLogic.onPostExecut(result);
+			}
+		}
+	}
+
+	public interface AsyncRefreshListener {
+		/**
+		 * notify listener when async refresh about to begin
+		 */
+		public void onAysncRefreshStarted();
+
+		/**
+		 * notify listener when async refresh ended
+		 */
+		public void onAysncRefreshEnded();
+
+	}
+
+	public interface BackgroundLogic {
+		/**
+		 * the real background logic
+		 * 
+		 * @return result from background logic
+		 */
+		public Object doInBackground();
+
+		/**
+		 * background job has done
+		 * 
+		 * @param result
+		 *            result from doInBackground
+		 */
+		public void onPostExecut(Object result);
+	}
+
+}
---
+++ warenixlib/src/org/dyndns/warenix/util/WLog.java
@@ -0,0 +1,29 @@
+package org.dyndns.warenix.util;
+
+import android.util.Log;
+
+public class WLog {
+
+	private static String APP_NAME;
+
+	public static void d(String TAG, String message) {
+		Log.d(TAG, getAppName() + ":" + message);
+	}
+
+	public static void i(String TAG, String message) {
+		Log.i(TAG, getAppName() + ":" + message);
+	}
+
+	private static String getAppName() {
+		if (APP_NAME == null) {
+			throw new IllegalStateException(
+					"Please call WLog.setAppName() before");
+		}
+		return APP_NAME;
+	}
+
+	public static void setAppName(String appName) {
+		APP_NAME = appName;
+	}
+
+}
